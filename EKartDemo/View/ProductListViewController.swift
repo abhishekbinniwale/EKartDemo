ffccbadf1e0bf9de0b96d576cc0ac10021b1903d
Abhishek Binniwale <abhishek.binniwale@mavenir.com>
2020-08-14 14:42:59 +0530

    initial set up commit

--- EKartDemo/View/ProductListViewController.swift
+++ EKartDemo/View/ProductListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProductListViewController.swift
 //  EKartDemo
 //
 //  Created by Abhishek Binniwale on 14/08/20.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProductListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
